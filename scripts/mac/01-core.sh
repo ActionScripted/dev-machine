c8134d9ffa4f04124d10f0ac31835888850cf352
Michael Thompson <actionscripted@gmail.com>
2022-09-04 00:45:04 -0400

    upgrading the docker stuff for unattended runs

--- scripts/mac/01-core.sh
+++ scripts/mac/01-core.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+##
 # Install: Command Line Tools for Xcode and Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+##
 # Install: Docker (Desktop)
 brew install --cask docker
+
+##
+# Setup: Docker (Desktop)
+docker_app_path="/Applications/Docker.app"
+sudo ${docker_app_path}/Contents/MacOS/Docker --unattended --install-privileged-components
+
+##
+# Run: Docker (Desktop)
+# The "docker" inside of the app appears when Docker Desktop is running.
+# https://github.com/docker/for-mac/issues/2359#issuecomment-943131345
+open -a "$docker_app_path" --args --unattended --accept-license
+while ! "$docker_app_path"/Contents/Resources/bin/docker info &>/dev/null; do sleep 1; done
